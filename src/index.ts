d210a2ced83f96a10ee76e5568d92f3782888392
Rudolph Razul <rudolphjose.razul@timesoftsg.com.sg>
2025-08-25 14:33:33 +0800

    updated api url, notes

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
   /*
-  Objective: Write a function that will call a public API and display the "Title", and "Publisher"
+  Objective: 
+    Write a function that will call a public API and display the "Title", and "Description". 
   e.g.
-    Title: Piedmont Ave. bulletin. [volume] by Publisher: Piedmont Avenue Merchants Association
-    Title: Piedmont Avenue District bulletin. [volume] by Publisher: Piedmont Press
-    Title: Piedmont District bulletin. [volume] by Publisher: Piedmont Press
+    Title: Piedmont Ave. bulletin. [volume] by Description: Piedmont Avenue Merchants Association 
  
   1. Examine the API (and its results), and design the required interaces or structure 
-  2. Write a function that will call a public API and display the "Title", and "Publisher"
+  2. Write a function that will call a public API and display the "Title", and "Description"
   
-  Note: you can use or "Import" your preferred  api data fetching library.
+  Note: 
+      Traverse through the nodes "content" then "results"
+      You can use or "Import" your preferred  api data fetching library.
   */
 
 import fetch from 'node-fetch';
  
 async function fetchAndDisplayResults() {
-  const apiUrl = 'https://chroniclingamerica.loc.gov/search/titles/results/?terms=oakland&format=json&page=5';
+  const apiUrl = 'https://www.loc.gov/collections/chronicling-america/?dl=title&fo=json&q=oakland&location_state=california';
 
   //your code here
 
   //note: use this format.
-  //console.log(`Title: ${item.title} by Publisher: ${item.publisher}`);
+  //console.log(`Title: ${item.title} by Description: ${item.Description}`);
 }
 
 fetchAndDisplayResults();
